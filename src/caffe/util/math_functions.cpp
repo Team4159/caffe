0ddd84d82ff429651b4270e5ae4d58fb7501474a
IamTechknow <techhexium@gmail.com>
2018-01-07 11:10:08 -0800

    Revert "Correct the assertion failure not ignore"
    This reverts commit 82f6a71061b3262a3ba7bbc81ea2345875671bc0.

--- src/caffe/util/math_functions.cpp
+++ src/caffe/util/math_functions.cpp
@@ -244,17 +244,10 @@ template
 double caffe_nextafter(const double b);
 
 template <typename Dtype>
-void caffe_rng_uniform(const int n, Dtype a, Dtype b, Dtype* r) {
+void caffe_rng_uniform(const int n, const Dtype a, const Dtype b, Dtype* r) {
   CHECK_GE(n, 0);
   CHECK(r);
-
-  if(a > b) { //Changed by Edmond, swap if assertion fails
-    Dtype c = a;
-    a = b;
-    b = c;
-  }
-  CHECK_LE(a, b);
-  
+  //CHECK_LE(a, b);
   boost::uniform_real<Dtype> random_distribution(a, caffe_nextafter<Dtype>(b));
   boost::variate_generator<caffe::rng_t*, boost::uniform_real<Dtype> >
       variate_generator(caffe_rng(), random_distribution);
